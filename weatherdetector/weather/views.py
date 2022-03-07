4758a1bebfd306cb0f0500ba11535ede7c11fadf
unknown <aylentorres27@gmail.com>
2022-03-07 20:43:27 -0300

    Añadiendo nuevos cambios

--- weatherdetector/weather/views.py
+++ weatherdetector/weather/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+import json
+import urllib.request
 
 # Create your views here.
+def index(request):
+    if request.metod == 'POST':
+        city = request.POST['city']
+        res = urllib.request.urlopen('http://api.openweathermap.org/data/2.5/weather?q='+city+'&appid=91013e8cb10bff4c467410fcf031917d').read()
+        json_data = json.loads(res)
+        data = {
+            'country_code': str(json_data['sys']['country']),
+            'coordinate': str(json_data['coord']['lon']) + ' ' + str(json_data['coord']['lat']),
+            'temp': str(json_data['main']['temp'])+'k,',
+            'pressure': str(json_data['main']['pressure']),
+            'humidity': str(json_data['main']['humidity']),
+        }
+    else:
+        data = {}
+    return render(request, 'index.html', data)
